cebf31bfa24e6837fa82e071cf3b09569b66341b
dinaraparanid <dinaraparanid@gmail.com>
2023-07-13 23:36:36 +0300

    unsafePerformIO removed

--- game/Game.hs
+++ game/Game.hs
@@ -8,6 +8,7 @@
 module Game where
 
 import           Apecs.Gloss
+import qualified Apecs.Gloss              as Gloss
 import           Client
 import           Control.Concurrent.Async as Async
 import           Control.Concurrent.STM   as STM
@@ -86,6 +87,21 @@ renderGame
       , translatedShotPic p2Shot
       ]
 renderGame _ _ _ = blank
+
+-- | Renders game level with all players and events that are happen.
+--   In case if second player has not connected,
+--   wait for 2-nd player window is shown.
+--   In case if either 1-st or 2-nd player has won,
+--   victory screen of particular player is shown.
+--   In other cases, typical game field is shown.
+--   This version is not differ from renderGame, just an IO wrapper
+
+renderGameIO ::
+  Picture        -- ^ first player picture (already rotated and translated)
+  -> Picture     -- ^ second player picture (already rotated and translated)
+  -> SystemState -- ^ current system state
+  -> IO Picture
+renderGameIO firstPlayerPic secondPlayerPic sys = return $ renderGame firstPlayerPic secondPlayerPic sys
 
 ----------------------- Message-Passing Wrappers --------------------------
 
@@ -298,7 +314,7 @@ handleDirectionEvent ::
   -> SystemState                 -- ^ current system state
   -> Word8                       -- ^ player's index (1 or 2)
   -> (Direction -> Direction)    -- ^ direction transformer
-  -> SystemState
+  -> IO SystemState
 handleDirectionEvent
   requestChan
   curSysStateHolder
@@ -306,14 +322,7 @@ handleDirectionEvent
   1 dirTransf = do
     let newP1Dir = dirTransf p1Dir
     let updDirsRequest = GameState.UpdateDirection (UpdateDirectionData 1 newP1Dir)
-
-    -- Не придумал ничего достаточно элегантного и простого, чтобы этого избежать.
-    -- Учитывая, что функция запускается реактивно, так или иначе придется использовать
-    -- примитивы синхронизации или атомарность, иначе это всё не потокобезопасно.
-    -- Другое решение, которое меня посетило - хранить запросы в SystemState,
-    -- но это решение так же не принесет ничего существенного из-за реактивности
-    -- (в чистых реактивных функциях всё равно придется вызывать IO/STM)
-    unsafePerformIO $ wrapP1DirectionWithIO
+    wrapP1DirectionWithIO
       requestChan curSysStateHolder newP1Dir p2Dir p1Crds p2Crds p1Shot p2Shot updDirsRequest
 
 handleDirectionEvent
@@ -323,12 +332,10 @@ handleDirectionEvent
   2 dirTransf = do
     let newP2Dir = dirTransf p2Dir
     let updDirsRequest = GameState.UpdateDirection (UpdateDirectionData 2 newP2Dir)
-
-    -- Про причины использования unsafePerformIO читать выше
-    unsafePerformIO $ wrapP2DirectionWithIO
+    wrapP2DirectionWithIO
       requestChan curSysStateHolder p1Dir newP2Dir p1Crds p2Crds p1Shot p2Shot updDirsRequest
 
-handleDirectionEvent _ _ state _ _ = state
+handleDirectionEvent _ _ state _ _ = return state
 
 -- | Updates positions for both players
 ---  by fetching them from the server
@@ -338,7 +345,7 @@ handleMovementEvent ::
   -> SystemState                  -- ^ current system state
   -> Word8                        -- ^ player's index (1 or 2)
   -> (Coordinates -> Coordinates) -- ^ position transformer
-  -> SystemState
+  -> IO SystemState
 handleMovementEvent
   requestChan
   curSysStateHolder
@@ -347,10 +354,10 @@ handleMovementEvent
   where
     firstPlayerState = (Just p1Dir, Just p1Crds, p1Shot)
     secondPlayerState = (Just p2Dir, Just p2Crds, p2Shot)
-    curState = (GameState [firstPlayerState, secondPlayerState], playerState)
+    curState = return (GameState [firstPlayerState, secondPlayerState], playerState)
     newP1Crds = crdTransf p1Crds
     updCrdsRequest = GameState.UpdatePosition $ UpdatePositionData 1 newP1Crds
-    nextState = unsafePerformIO $ wrapP1MovementWithIO -- О причинах использования unsafePerformIO читать выше
+    nextState = wrapP1MovementWithIO
       requestChan curSysStateHolder p1Dir p2Dir newP1Crds p2Crds p1Shot p2Shot updCrdsRequest
 
 handleMovementEvent
@@ -361,13 +368,13 @@ handleMovementEvent
   where
     firstPlayerState = (Just p1Dir, Just p1Crds, p1Shot)
     secondPlayerState = (Just p2Dir, Just p2Crds, p2Shot)
-    curState = (GameState [firstPlayerState, secondPlayerState], playerState)
+    curState = return (GameState [firstPlayerState, secondPlayerState], playerState)
     newP2Crds = crdTransf p2Crds
     updCrdsRequest = GameState.UpdatePosition $ UpdatePositionData 2 newP2Crds
-    nextState = unsafePerformIO $ wrapP2MovementWithIO -- О причинах использования unsafePerformIO читать выше
+    nextState = wrapP2MovementWithIO
       requestChan curSysStateHolder p1Dir p2Dir p1Crds newP2Crds p1Shot p2Shot updCrdsRequest
 
-handleMovementEvent _ _ state _ _ = state
+handleMovementEvent _ _ state _ _ = return state
 
 -- | Updates shot states for both players
 --   by fetching them from the server
@@ -377,7 +384,7 @@ handleShootEvent ::
   -> SystemState                  -- ^ current system state
   -> Word8                        -- ^ player's index (1 or 2)
   -> (Maybe ShotData -> ShotData) -- ^ shot data's transformer
-  -> SystemState
+  -> IO SystemState
 handleShootEvent
   requestChan
   curSysStateHolder
@@ -385,13 +392,12 @@ handleShootEvent
   1 shotTransf = if beyoundBorders crds then noShotState else shootState
   where
     cancelShootRequest = GameState.CancelShoot 1
-    noShotState = unsafePerformIO $ wrapP1CancelShotWithIO
+    noShotState = wrapP1CancelShotWithIO
       requestChan curSysStateHolder p1Dir p2Dir p1Crds p2Crds p2Shot cancelShootRequest
-
     newP1Shot = shotTransf p1Shot
     (ShotData _ crds _) = newP1Shot
     shootRequest = GameState.Shoot newP1Shot
-    shootState = unsafePerformIO $ wrapP1ShotWithIO -- О причинах использования unsafePerformIO читать выше
+    shootState = wrapP1ShotWithIO
       requestChan curSysStateHolder p1Dir p2Dir p1Crds p2Crds newP1Shot p2Shot shootRequest
 
 handleShootEvent
@@ -401,16 +407,15 @@ handleShootEvent
   2 shotTransf = if beyoundBorders crds then noShotState else nextState
   where
     cancelShootRequest = GameState.CancelShoot 2
-    noShotState = unsafePerformIO $ wrapP2CancelShotWithIO
+    noShotState = wrapP2CancelShotWithIO
       requestChan curSysStateHolder p1Dir p2Dir p1Crds p2Crds p1Shot cancelShootRequest
-
     newP2Shot = shotTransf p2Shot
     (ShotData _ crds _) = newP2Shot
     shootRequest = GameState.Shoot newP2Shot
-    nextState = unsafePerformIO $ wrapP2ShotWithIO -- О причинах использования unsafePerformIO читать выше
+    nextState = wrapP2ShotWithIO
       requestChan curSysStateHolder p1Dir p2Dir p1Crds p2Crds p1Shot newP2Shot shootRequest
 
-handleShootEvent _ _ state _ _ = state
+handleShootEvent _ _ state _ _ = return state
 
 -- | Gets next player's direction after key was pressed
 onLeftRightEvent ::
@@ -434,7 +439,7 @@ handleGameEvents ::
   -> TVar SystemState            -- ^ atomic holder for a current system's state
   -> Event                       -- ^ event that triggered the update
   -> SystemState                 -- ^ current system's state
-  -> SystemState
+  -> IO SystemState
 ------------------------------------------ On Up pressed ------------------------------------------
 handleGameEvents
   requestChan
@@ -575,7 +580,7 @@ handleGameEvents
       Nothing       -> ShotData playerInd (Maybe.fromJust crds) (Maybe.fromJust dir)
       Just shotData -> shotData
 
-handleGameEvents _ _ _ state = state
+handleGameEvents _ _ _ state = return state
 
 -- | Updates shot position on single time unit passed
 updateShot :: Maybe ShotData -> Either ShotData UpdateShotError
@@ -596,14 +601,14 @@ updateGame ::
   -> TVar SystemState               -- ^ atomic holder for a current system's state
   -> Float                          -- ^ current time passed from the start (unused)
   -> SystemState                    -- ^ current system's state
-  -> SystemState
+  -> IO SystemState
 updateGame eventQueue requestChan curSysStateHolder _
   (GameState [(p1Dir, p1Crds, p1Shot), (p2Dir, p2Crds, p2Shot)], PlayerState (Just 1) dir crds) =
   case fetchedState of
     (GameState [(_, _, fP1Shot), (_, _, fP2Shot)], _) -> do
       let newP1Shot = updateShot fP1Shot
       updateGame1_ requestChan curSysStateHolder fetchedState newP1Shot fP2Shot
-    state -> state
+    state -> return state
 
   where
     curPlayerState = PlayerState (Just 1) dir crds
@@ -616,15 +621,18 @@ updateGame eventQueue requestChan curSysStateHolder _
     (GameState [(_, _, fP1Shot), (_, _, fP2Shot)], _) -> do
       let newP2Shot = updateShot fP2Shot
       updateGame2_ requestChan curSysStateHolder fetchedState fP1Shot newP2Shot
-    state -> state
+    state -> return state
 
   where
     curPlayerState = PlayerState (Just 2) dir crds
     curState = (GameState [(p1Dir, p1Crds, p1Shot), (p2Dir, p2Crds, p2Shot)], curPlayerState)
     fetchedState = Maybe.fromMaybe curState $ unsafePerformIO $ STM.atomically $ STM.tryReadTChan eventQueue
 
-updateGame eventQueue _ _ _ curState =
-  Maybe.fromMaybe curState $ unsafePerformIO $ STM.atomically $ STM.tryReadTChan eventQueue
+updateGame eventQueue _ _ _ curState = do
+  nextStateMb <- STM.atomically $ STM.tryReadTChan eventQueue
+  case nextStateMb of
+    Just state -> return state
+    Nothing    -> return curState
 
 -- | Updates 1-st player's shot state and sends it to the server
 updateGame1_ ::
@@ -633,14 +641,13 @@ updateGame1_ ::
   -> SystemState                     -- ^ fetched system's state
   -> Either ShotData UpdateShotError -- ^ 1-st player's shot data, updated after a time
   -> Maybe ShotData                  -- ^ current 2-nd player's shot data
-  -> SystemState
+  -> IO SystemState
 updateGame1_
   requestChan
   curSysStateHolder
   (GameState [(Just fP1Dir, Just fP1Crds, _), (Just fP2Dir, Just fP2Crds, _)], _)
   (Left newP1Shot)
-  newP2Shot = unsafePerformIO
-    $ wrapP1ShotWithIO requestChan curSysStateHolder fP1Dir fP2Dir fP1Crds fP2Crds newP1Shot newP2Shot
+  newP2Shot = wrapP1ShotWithIO requestChan curSysStateHolder fP1Dir fP2Dir fP1Crds fP2Crds newP1Shot newP2Shot
     $ GameState.Shoot newP1Shot
 
 updateGame1_
@@ -648,11 +655,10 @@ updateGame1_
   curSysStateHolder
   (GameState [(Just fP1Dir, Just fP1Crds, _), (Just fP2Dir, Just fP2Crds, _)], _)
   (Right GameState.OutOfBorders)
-  newP2Shot = unsafePerformIO
-    $ wrapP1CancelShotWithIO requestChan curSysStateHolder fP1Dir fP2Dir fP1Crds fP2Crds newP2Shot
+  newP2Shot = wrapP1CancelShotWithIO requestChan curSysStateHolder fP1Dir fP2Dir fP1Crds fP2Crds newP2Shot
     $ GameState.CancelShoot 1
 
-updateGame1_ _ _ fetchedState _ _ = fetchedState
+updateGame1_ _ _ fetchedState _ _ = return fetchedState
 
 -- | Updates 2-nd player's shot state and sends it to the server
 updateGame2_ ::
@@ -661,14 +667,14 @@ updateGame2_ ::
   -> SystemState                     -- ^ fetched system's state
   -> Maybe ShotData                  -- ^ current 1-st player's shot data
   -> Either ShotData UpdateShotError -- ^ 2-nd player's shot data, updated after a time
-  -> SystemState
+  -> IO SystemState
 updateGame2_
   requestChan
   curSysStateHolder
   (GameState [(Just fP1Dir, Just fP1Crds, _), (Just fP2Dir, Just fP2Crds, _)], _)
   newP1Shot
-  (Left newP2Shot) = unsafePerformIO
-    $ wrapP2ShotWithIO requestChan curSysStateHolder fP1Dir fP2Dir fP1Crds fP2Crds newP1Shot newP2Shot
+  (Left newP2Shot) = wrapP2ShotWithIO
+    requestChan curSysStateHolder fP1Dir fP2Dir fP1Crds fP2Crds newP1Shot newP2Shot
     $ GameState.Shoot newP2Shot
 
 updateGame2_
@@ -676,11 +682,11 @@ updateGame2_
   curSysStateHolder
   (GameState [(Just fP1Dir, Just fP1Crds, _), (Just fP2Dir, Just fP2Crds, _)], _)
   newP1Shot
-  (Right GameState.OutOfBorders) = unsafePerformIO
-    $ wrapP2CancelShotWithIO requestChan curSysStateHolder fP1Dir fP2Dir fP1Crds fP2Crds newP1Shot
+  (Right GameState.OutOfBorders) = wrapP2CancelShotWithIO
+    requestChan curSysStateHolder fP1Dir fP2Dir fP1Crds fP2Crds newP1Shot
     $ GameState.CancelShoot 2
 
-updateGame2_ _ _ fetchedState _ _ = fetchedState
+updateGame2_ _ _ fetchedState _ _ = return fetchedState
 
 -- | Launches game rendering and update handling
 launchGame ::
@@ -690,12 +696,12 @@ launchGame ::
   -> Picture                        -- ^ first player picture (already rotated and translated)
   -> Picture                        -- ^ second player picture (already rotated and translated)
   -> IO ()
-launchGame eventQueue requestChan curSysStateHolder firstPlayerPic secondPlayerPic = Gloss.play
+launchGame eventQueue requestChan curSysStateHolder firstPlayerPic secondPlayerPic = Gloss.playIO
   mainWindow
   white
   fps
   initialSystemState
-  (renderGame firstPlayerPic secondPlayerPic)
+  (renderGameIO firstPlayerPic secondPlayerPic)
   (handleGameEvents requestChan curSysStateHolder)
   (updateGame eventQueue requestChan curSysStateHolder)
 
